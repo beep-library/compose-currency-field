10517992f8128402cfd24430de15cb422c0a08ce
mangbaam <pmb0836@gmail.com>
2023-01-24 03:41:35 +0900

    feat: 예제 코드 추가

--- app/src/main/java/com/mangbaam/library/MainActivity.kt
+++ app/src/main/java/com/mangbaam/library/MainActivity.kt
@@ -1,12 +1,19 @@
 package com.mangbaam.library
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
 import com.mangbaam.library.ui.theme.CurrencyFieldTheme
 
 class MainActivity : ComponentActivity() {
@@ -18,8 +25,26 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
+                    CurrencyField(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(8.dp)
+                            .background(Color.LightGray),
+                        textStyle = MaterialTheme.typography.bodyLarge.copy(textAlign = TextAlign.End),
+                        onTextChanged = {
+                            Log.d(TAG, "onTextChanged: $it")
+                        }, onValueChanged = {
+                            Log.d(TAG, "onValueChanged: $it")
+                        },
+                        rearUnit = true,
+                        unit = "☻"
+                    )
                 }
             }
         }
+    }
+
+    companion object {
+        const val TAG = "로그"
     }
 }
